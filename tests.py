0c8b5973cacd7048737eb43072c7fef66355782e
Кирилл Гольцер <kirill.holzer@yandex.ru>
2023-06-12 15:23:57 +0300

    ADDED: test_add_book_same_book_two_times_false

--- tests.py
+++ tests.py
@@ -9,6 +9,12 @@ class TestBooksCollector:
         collector.add_new_book('Горе от ума')
         assert collector.books_rating == {'Горе от ума': 1}
 
+    def test_add_book_same_book_two_times_false(self):
+        collector = BooksCollector()
+        collector.add_new_book('Крёстный отец')
+        collector.add_new_book('Крёстный отец')
+        assert collector.books_rating == {'Крёстный отец': 1}
+
     def test_set_book_rating_less_than_one_false(self):
         collector = BooksCollector()
         collector.add_new_book('Властелин Колец')
@@ -24,6 +30,8 @@ class TestBooksCollector:
     def test_get_book_rating_not_in_list_false(self):
         collector = BooksCollector()
         assert collector.get_book_rating('Капитанская дочка') == None
+
+
 
 
 

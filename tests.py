7640cd2d676b3cd13784c04f15d5ea3845f9c016
Кирилл Гольцер <kirill.holzer@yandex.ru>
2023-06-12 14:51:17 +0300

    ADED: test_set_book_rating_less_than_one_false

--- tests.py
+++ tests.py
@@ -8,5 +8,14 @@ class TestBooksCollector:
         collector.add_new_book('Горе от ума')
         assert collector.books_rating == {'Горе от ума': 1}
 
+    def test_set_book_rating_less_than_one_false(self):
+        collector = BooksCollector()
+        collector.add_new_book('Властелин Колец')
+        collector.set_book_rating('Властелин Колец', 0)
+        assert collector.books_rating == {'Властелин Колец': 1}
+
+
+
+
 
 

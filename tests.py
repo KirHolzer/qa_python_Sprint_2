309b60f4c86d7ee52ca4abaa9a1c57d2954188f1
Кирилл Гольцер <kirill.holzer@yandex.ru>
2023-06-12 14:54:34 +0300

    test_set_book_rating_more_than_ten_false

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
+# pytest -v tests.py
 class TestBooksCollector:
     def test_add_book_true(self):
         collector = BooksCollector()
@@ -14,6 +15,11 @@ class TestBooksCollector:
         collector.set_book_rating('Властелин Колец', 0)
         assert collector.books_rating == {'Властелин Колец': 1}
 
+    def test_set_book_rating_more_than_ten_false(self):
+        collector = BooksCollector()
+        collector.add_new_book('Властелин Колец')
+        collector.set_book_rating('Властелин Колец', 11)
+        assert collector.books_rating == {'Властелин Колец': 1}
 
 
 
